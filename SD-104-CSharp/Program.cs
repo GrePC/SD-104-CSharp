11d1f6260bc8841c2b1a865d0f86f0355ebeab85
GrePC <64420953+GrePC@users.noreply.github.com>
2020-05-11 00:46:14 -0500

    Add project files.

---
+++ SD-104-CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SD_104_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Name Is Tigre Anthony AKA Gre.");
+        }
+    }
+}
